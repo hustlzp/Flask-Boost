f59bc2bba868b996fd1350cc964e730e1a7210dd
hustlzp <hustlzp@qq.com>
2014-08-17 12:18:41 +0800

    update fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,19 @@
 from fabric.api import run, env, cd, prefix
-from proj import config
+from config import load_config
+
+config = load_config()
+host_string = config.HOST_STRING
 
 
 def deploy():
     env.host_string = config.HOST_STRING
     with cd('/var/www/proj'):
+        run('git reset --hard HEAD')
         run('git pull')
         run('bower install')
         with prefix('source venv/bin/activate'):
             run('pip install -r requirements.txt')
+            run('python manage.py db upgrade')
         run('sudo supervisorctl restart proj')
 
 
