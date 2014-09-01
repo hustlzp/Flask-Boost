9147a5cf55a9923fde5debffa84fffaa7e5246f9
hustlzp <hustlzp@qq.com>
2014-09-02 00:38:17 +0800

    update nginx.conf

--- config/default.py
+++ config/default.py
@@ -9,13 +9,13 @@ class Config(object):
     TESTING = False
     SECRET_KEY = "\xb5\xb3}#\xb7A\xcac\x9d0\xb6\x0f\x80z\x97\x00\x1e\xc0\xb8+\xe9)\xf0}"
     PERMANENT_SESSION_LIFETIME = 3600 * 24 * 7
-    SESSION_COOKIE_NAME = 'tm_session'
+    SESSION_COOKIE_NAME = 'proj_session'
 
     # Root path of project
     PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
     # Site domain
-    SITE_TITLE = "Project"
+    SITE_TITLE = "proj"
     SITE_DOMAIN = "http://localhost:5000"
 
     # SQLAlchemy config
@@ -47,4 +47,4 @@ class Config(object):
     SENTRY_DSN = ''
 
     # Host string, used by fabric
-    HOST_STRING = "root@114.215.203.104"
+    HOST_STRING = "root@12.34.56.78"
