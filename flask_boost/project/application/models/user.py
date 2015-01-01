3bc8a7208865bac6364ce65410dd828e576c30c1
hustlzp <hustlzp@qq.com>
2015-01-01 09:35:58 +0800

    Add unique constraint to User.email

--- flask_boost/project/application/models/user.py
+++ flask_boost/project/application/models/user.py
@@ -6,7 +6,7 @@ from ._base import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True)
-    email = db.Column(db.String(50))
+    email = db.Column(db.String(50), unique=True)
     avatar = db.Column(db.String(200))
     password = db.Column(db.String(200))
     created_at = db.Column(db.DateTime, default=datetime.datetime.now)
