90f893b4eeaece5f2fb550bdd5bf4209a065f0fb
hustlzp <hustlzp@qq.com>
2015-01-01 12:33:19 +0800

    Now can signup.

--- flask_boost/project/application/controllers/account.py
+++ flask_boost/project/application/controllers/account.py
@@ -1,8 +1,9 @@
 # coding: utf-8
 from flask import render_template, Blueprint, redirect, request, url_for
-from ..forms import SigninForm
+from ..forms import SigninForm, SignupForm
 from ..utils.account import signin_user, signout_user
 from ..utils.permissions import VisitorPermission
+from ..models import db, User
 
 bp = Blueprint('account', __name__)
 
@@ -22,7 +23,16 @@ def signin():
 @VisitorPermission()
 def signup():
     """Signup"""
-    pass
+    form = SignupForm()
+    if form.validate_on_submit():
+        params = form.data.copy()
+        params.pop('repassword')
+        user = User(**params)
+        db.session.add(user)
+        db.session.commit()
+        signin_user(user)
+        return redirect(url_for('site.index'))
+    return render_template('account/signup.html', form=form)
 
 
 @bp.route('/signout')
--- flask_boost/project/application/forms/account.py
+++ flask_boost/project/application/forms/account.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 from flask_wtf import Form
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, EqualTo
 from ..models import User
 
 
@@ -31,3 +31,38 @@ class SigninForm(Form):
                 raise ValueError('密码错误')
             else:
                 self.user = user
+
+
+class SignupForm(Form):
+    """Form for signin"""
+    name = StringField('用户名',
+                       validators=[DataRequired('用户名不能为空')],
+                       description='用户名')
+
+    email = StringField('邮箱',
+                        validators=[
+                            DataRequired(message="邮箱不能为空"),
+                            Email(message="无效的邮箱")
+                        ],
+                        description='邮箱')
+
+    password = PasswordField('密码',
+                             validators=[DataRequired('密码不能为空')],
+                             description='密码')
+
+    repassword = PasswordField('重复密码',
+                               validators=[
+                                   DataRequired('重复密码不能为空'),
+                                   EqualTo('password', message='两次输入密码不一致')
+                               ],
+                               description='重复密码')
+
+    def validate_name(self, field):
+        user = User.query.filter(User.name == self.name.data).first()
+        if user:
+            raise ValueError('用户名已存在')
+
+    def validate_email(self, field):
+        user = User.query.filter(User.email == self.email.data).first()
+        if user:
+            raise ValueError('邮箱已被注册')
