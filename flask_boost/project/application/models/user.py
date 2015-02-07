f13c08b2793b72f373785d2fa5b004ec79da93d6
hustlzp <hustlzp@qq.com>
2015-02-08 00:19:22 +0800

    Add avatar_url property to User.

--- flask_boost/project/application/models/user.py
+++ flask_boost/project/application/models/user.py
@@ -2,13 +2,14 @@
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from ._base import db
+from ..utils.uploadsets import avatars
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True)
     email = db.Column(db.String(50), unique=True)
-    avatar = db.Column(db.String(200))
+    avatar = db.Column(db.String(200), default='default.png')
     password = db.Column(db.String(200))
     is_admin = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, default=datetime.now)
@@ -21,6 +22,10 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+    @property
+    def avatar_url(self):
+        return avatars.url(self.avatar)
 
     def __repr__(self):
         return '<User %s>' % self.name
