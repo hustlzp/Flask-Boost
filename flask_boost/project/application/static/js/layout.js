ecdf740401416e90a92f739140ee6e7a84fde902
hustlzp <hustlzp@qq.com>
2015-02-11 16:54:15 +0800

    Add csrf token header for Ajax request.

--- flask_boost/project/application/static/js/layout.js
+++ flask_boost/project/application/static/js/layout.js
@@ -1,3 +1,13 @@
+// Add csrf token header for Ajax request
+var csrf_token = $("meta[name='csrf-token']").attr('content');
+$.ajaxSetup({
+    beforeSend: function (xhr, settings) {
+        if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type)) {
+            xhr.setRequestHeader("X-CSRFToken", csrf_token);
+        }
+    }
+});
+
 // Flash message
 setTimeout(showFlash, 200);
 setTimeout(hideFlash, 2000);
