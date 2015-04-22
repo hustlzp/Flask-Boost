56449cb7a46ab126085c79fbeb546e307425e39b
hustlzp <hustlzp@qq.com>
2015-04-22 10:27:33 +0800

    Insert project root path to sys.path for dev.

--- flask_boost/cli.py
+++ flask_boost/cli.py
@@ -14,7 +14,14 @@ Options:
   -v, --version       Show version.
 """
 
+import sys
 import os
+
+# Insert project root path to sys.path
+project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if project_path not in sys.path:
+    sys.path.insert(0, project_path)
+
 import logging
 from logging import StreamHandler, DEBUG
 from os.path import dirname, abspath
