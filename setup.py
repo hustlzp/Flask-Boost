922fafcccbbe90759593fbfab94f17d1bfdb2c73
hustlzp <hustlzp@qq.com>
2015-04-29 08:52:21 +0800

    Rm Python3 flag.

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
