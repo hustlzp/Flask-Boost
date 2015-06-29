e89c023666e59778aea1c29c224d368ad28ff741
hustlzp <hustlzp@qq.com>
2015-06-29 10:19:31 +0800

    Update fab deploy.

--- flask_boost/project/fabfile.py
+++ flask_boost/project/fabfile.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from fabric.api import run, env, cd, prefix, shell_env
+from fabric.api import run, env, cd, prefix, shell_env, local
 from config import load_config
 
 config = load_config()
@@ -7,7 +7,6 @@ host_string = config.HOST_STRING
 
 
 def deploy():
-    """部署"""
     env.host_string = config.HOST_STRING
     with cd('/var/www/#{project}'):
         with shell_env(MODE='PRODUCTION'):
@@ -16,11 +15,10 @@ def deploy():
             with prefix('source venv/bin/activate'):
                 run('pip install -r requirements.txt')
                 run('python manage.py db upgrade')
-                run('python manage.py build_assets')
+                run('python manage.py build')
             run('supervisorctl restart #{project}')
 
 
 def restart():
-    """重启"""
     env.host_string = config.HOST_STRING
     run('supervisorctl restart #{project}')
