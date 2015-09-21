bb2e2a3ea7fcaf37df8a13298708c207189b0c7e
hustlzp <hustlzp@qq.com>
2015-09-21 18:44:32 +0800

    Update fab deploy.

--- flask_boost/project/fabfile.py
+++ flask_boost/project/fabfile.py
@@ -12,6 +12,8 @@ def deploy():
         with shell_env(MODE='PRODUCTION'):
             run('git reset --hard HEAD')
             run('git pull')
+            run('npm install')
+            run('gulp')
             with prefix('source venv/bin/activate'):
                 run('pip install -r requirements.txt')
                 run('python manage.py db upgrade')
