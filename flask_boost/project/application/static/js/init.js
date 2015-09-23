87683c8ff02983a953f615ffc203a8d9df098484
hustlzp <hustlzp@qq.com>
2015-09-23 17:03:58 +0800

    Move AJAX setup codes to layout.js.

--- flask_boost/project/application/static/js/init.js
+++ flask_boost/project/application/static/js/init.js
@@ -1,15 +1,6 @@
 (function () {
     "use strict";
 
-    // Add CSRF token header for Ajax request
-    $.ajaxSetup({
-        beforeSend: function (xhr, settings) {
-            if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type)) {
-                xhr.setRequestHeader("X-CSRFToken", g.csrfToken);
-            }
-        }
-    });
-
     // Find out params in routing rules
     var pattern = new RegExp("<[^:]*:?([^>]+)>", "g");
     var result = null;
--- flask_boost/project/application/static/js/layout.js
+++ flask_boost/project/application/static/js/layout.js
@@ -1,6 +1,15 @@
 (function () {
     "use strict";
 
+    // Add CSRF token header for Ajax request
+    $.ajaxSetup({
+        beforeSend: function (xhr, settings) {
+            if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type)) {
+                xhr.setRequestHeader("X-CSRFToken", g.csrfToken);
+            }
+        }
+    });
+
     // Flash message
     setTimeout(showFlash, 200);
     setTimeout(hideFlash, 2000);
