dfed48001404e98ee6c165746116dfbcdf213708
hustlzp <hustlzp@qq.com>
2015-10-27 11:02:23 +0800

    Replace shutil.move with shutil.copy.

--- flask_boost/cli.py
+++ flask_boost/cli.py
@@ -317,8 +317,8 @@ def _rewrite_and_copy(src_file, dst_file, project_name):
                     replace('#{project|title}', project_name.title())
                 new_file.write(new_line)
 
-    # Move to new file
-    shutil.move(abs_path, dst_file)
+    # Copy to new file
+    shutil.copy(abs_path, dst_file)
     os.close(fh)
 
 
