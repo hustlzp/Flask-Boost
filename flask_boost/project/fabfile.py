a7240d8710cd9fc763514fd7aa92e4a1141119e6
hustlzp <hustlzp@qq.com>
2015-11-06 21:50:39 +0800

    Update fab deploy.

--- flask_boost/project/fabfile.py
+++ flask_boost/project/fabfile.py
@@ -17,6 +17,7 @@ def deploy():
             with prefix('source venv/bin/activate'):
                 run('pip install -r requirements.txt')
                 run('python manage.py db upgrade')
+                run('gulp')
                 run('python manage.py build')
             run('supervisorctl restart #{project}')
 
